e3962b6213f08f17a3fc634adc28e053638522d9
marco76tv <marco.sottana@gmail.com>
2025-10-10 16:42:46 +0200

    .

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -20,21 +20,7 @@ class TestCommand extends Command
      *
      * @return void
      */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     public function handle(): void
-    public function handle()
->>>>>>> f057083 (.)
-=======
-    public function handle()
->>>>>>> fafcd56 (.)
-=======
-    public function handle()
->>>>>>> 754a996 (.)
-=======
-    public function handle()
->>>>>>> 8622c2a (.)
     {
         $name = TenantService::getName();
         $this->info('tenant name :' . $name);
