eca9207510d245c6a2657d4301c3381e40173e9e
marco76tv <marco.sottana@gmail.com>
2025-10-21 09:03:32 +0200

    .

--- app/Models/Traits/SushiToJsons.php
+++ app/Models/Traits/SushiToJsons.php
@@ -99,6 +99,14 @@ trait SushiToJsons
     }
 
     /**
+     * @return ?string
+     */
+    public function getConnectionName()
+    {
+        return parent::getConnectionName();
+    }
+
+    /**
      * bootUpdater function.
      */
     protected static function bootSushiToJsons(): void
