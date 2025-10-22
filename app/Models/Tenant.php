95e29cc2a2db853d5a64dd9390c6b6469f0d0fa2
marco76tv <marco.sottana@gmail.com>
2025-10-22 10:09:26 +0200

    .

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -19,7 +19,6 @@ use Str;
 /**
  * Modello Tenant per la gestione multi-tenant dell'applicazione.
  *
- * @property int $id
  * @property string $name
  * @property string $domain
  * @property string $database
@@ -27,21 +26,6 @@ use Str;
  * @property array|null $settings
  * @property bool $is_active
  * @property string|null $logo
- * @property string|null $email
- * @property string|null $phone
- * @property string|null $address
- * @property string|null $city
- * @property string|null $postal_code
- * @property string|null $province
- * @property string|null $country
- * @property string|null $tax_code
- * @property string|null $vat_number
- * @property int|null $owner_id
- * @property string|null $status
- * @property \Illuminate\Support\Carbon|null $last_activity_at
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property-read string $url
  * @property-read Collection<int, User> $users
  * @property-read int|null $users_count
