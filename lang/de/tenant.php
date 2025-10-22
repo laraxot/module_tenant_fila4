38dd5a7a505a5f2670d969be9ae214c6a26e5921
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:30:35 +0200

    .

--- lang/de/tenant.php
+++ lang/de/tenant.php
@@ -13,6 +13,20 @@ return [
             'label' => 'Multi-tenant',
         ],
     ],
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     'sections' => [
         'building' => [
             'navigation' => [
@@ -34,6 +48,20 @@ return [
                 'notes' => 'Notizen',
             ],
         ],
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
         'unit' => [
             'navigation' => [
                 'name' => 'Wohneinheiten',
@@ -61,6 +89,20 @@ return [
                 'warehouse' => 'Lager',
             ],
         ],
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
         'tenant' => [
             'navigation' => [
                 'name' => 'Mieter',
@@ -84,6 +126,20 @@ return [
             ],
         ],
     ],
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     'common' => [
         'status' => [
             'active' => 'Aktiv',
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Tests;
 
+<<<<<<< HEAD
 use Illuminate\Foundation\Application;
 use Modules\Tenant\Providers\TenantServiceProvider;
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+use Illuminate\Foundation\Application;
+use Modules\Tenant\Providers\TenantServiceProvider;
+=======
+use Modules\Tenant\Providers\TenantServiceProvider;
+use Illuminate\Foundation\Application;
+>>>>>>> a12f125f4a (.)
+=======
+use Illuminate\Foundation\Application;
+use Modules\Tenant\Providers\TenantServiceProvider;
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Modules\Xot\Tests\CreatesApplication;
 
@@ -25,7 +43,23 @@ abstract class TestCase extends BaseTestCase
 
         // Load Tenant module specific configurations
         $this->loadLaravelMigrations();
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+        
+>>>>>>> a12f125f4a (.)
+=======
+
+>>>>>>> b93ef594b4 (.)
+=======
+        
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
         // Seed any required data for Tenant tests
         $this->artisan('module:seed', ['module' => 'Tenant']);
     }
@@ -33,13 +67,29 @@ abstract class TestCase extends BaseTestCase
     /**
      * Get package providers.
      *
+<<<<<<< HEAD
      * @param Application $app
+=======
+<<<<<<< HEAD
+     * @param Application $app
+=======
+     * @param \Illuminate\Foundation\Application $app
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
      * @return array<int, class-string>
      */
     protected function getPackageProviders($app): array
     {
         return [
+<<<<<<< HEAD
             TenantServiceProvider::class,
+=======
+<<<<<<< HEAD
+            TenantServiceProvider::class,
+=======
+            \Modules\Tenant\Providers\TenantServiceProvider::class,
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
         ];
     }
 }
