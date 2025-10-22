a4745569a6a7de4c3ee4692f1140de54fe0740a9
marco76tv <marco.sottana@gmail.com>
2025-10-22 08:01:39 +0200

    .

--- app/Models/BaseModelJsons.php
+++ app/Models/BaseModelJsons.php
@@ -9,11 +9,7 @@ use Modules\Tenant\Models\Traits\SushiToJsons;
 /**
  * Class BaseModelJsons.
  *
-<<<<<<< HEAD
  * @property array $form
-=======
- * @property array $schema
->>>>>>> 7e38bd4 (.)
  */
 abstract class BaseModelJsons extends BaseModel
 {
