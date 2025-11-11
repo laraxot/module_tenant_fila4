51fab972d718fbaf89afa295b5cfcadd0014901b
marco76tv <marco.sottana@gmail.com>
2025-11-11 10:57:48 +0100

    .

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,7 +10,7 @@ $finder = PhpCsFixer\Finder::create()
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true)
+    ->ignoreVCS(true);
 
 $config = new PhpCsFixer\Config();
 
