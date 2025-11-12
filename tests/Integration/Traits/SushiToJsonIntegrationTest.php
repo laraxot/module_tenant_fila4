930f410fa79ca4b978f3633f662c1331aa910fb9
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:13:04 +0100

    .

--- tests/Integration/Traits/SushiToJsonIntegrationTest.php
+++ tests/Integration/Traits/SushiToJsonIntegrationTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Tests\Integration\Traits;
 
+use function Safe\json_decode;
+use function Safe\json_encode;
+
+
 use Illuminate\Support\Facades\File;
 use Modules\Tenant\Models\Tenant;
 use Modules\Tenant\Models\TestSushiModel;
@@ -199,9 +203,9 @@ class SushiToJsonIntegrationTest extends TestCase
     public function it_handles_concurrent_access_safely(): void
     {
         // Simula accesso concorrente creando più istanze del modello
-        $model1 = new TestSushiModel;
-        $model2 = new TestSushiModel;
-        $model3 = new TestSushiModel;
+        $model1 = new TestSushiModel();
+        $model2 = new TestSushiModel();
+        $model3 = new TestSushiModel();
 
         $testData1 = ['1' => ['id' => 1, 'name' => 'Concurrent Item 1']];
         $testData2 = ['2' => ['id' => 2, 'name' => 'Concurrent Item 2']];
@@ -365,7 +369,7 @@ class SushiToJsonIntegrationTest extends TestCase
         // Imposta il secondo tenant come corrente
         app('tenant')->setCurrent($secondTenant);
 
-        $secondModel = new TestSushiModel;
+        $secondModel = new TestSushiModel();
         $secondJsonPath = TenantService::filePath('database/content/test_sushi.json');
 
         $testData = [
