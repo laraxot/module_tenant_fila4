d4cb7b05fbd0c4619cbb7516031a6276e0c0f860
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:27:06 +0100

    .

--- app/Models/Traits/SushiToCsv.php
+++ app/Models/Traits/SushiToCsv.php
@@ -20,9 +20,6 @@ trait SushiToCsv
 {
     use Sushi;
 
-    /**
-     * @return array<string, mixed>
-     */
     public function getSushiRows(): array
     {
         // return CSV::fromFile(__DIR__.'/roles.csv')->toArray();
@@ -48,9 +45,6 @@ trait SushiToCsv
         return $path;
     }
 
-    /**
-     * @return array<string, mixed>
-     */
     public function getCsvHeader(): array
     {
         $reader = Reader::createFromPath($this->getCsvPath(), 'r');
