da1343b67537da507cc05ff0042a6114c733f7a0
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:23:48 +0100

    .

--- app/Models/TestSushiModel.php
+++ app/Models/TestSushiModel.php
@@ -51,11 +51,7 @@ class TestSushiModel extends Model
      *
      * @var array<string, string>
      */
-<<<<<<< HEAD
     protected $form = [
-=======
-    protected $schema = [
->>>>>>> 7e38bd4 (.)
         'id' => 'integer',
         'name' => 'string',
         'description' => 'string',
