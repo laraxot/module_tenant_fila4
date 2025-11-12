782fab4e686acfd442b00ab681471b0ce1dcb89e
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:26:23 +0100

    .

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,6 @@
 declare(strict_types=1);
 
 /*
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> b13ae59 (.)
  * |--------------------------------------------------------------------------
  * | API Routes
  * |--------------------------------------------------------------------------
@@ -20,43 +12,8 @@ declare(strict_types=1);
  * | is assigned the "api" middleware group. Enjoy building your API!
  * |
  */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b13ae59 (.)
 /*
  * Route::middleware('auth:api')->get('/tenant', function (Request $request) {
  * return $request->user();
  * });
  */
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-/*
-Route::middleware('auth:api')->get('/tenant', function (Request $request) {
-    return $request->user();
-});
-*/
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-/*
- * Route::middleware('auth:api')->get('/tenant', function (Request $request) {
- * return $request->user();
- * });
- */
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
