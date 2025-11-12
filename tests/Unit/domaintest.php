bc43f71badf9cd379922464bcdf44d7f464cb69a
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:22:39 +0100

    .

--- tests/Unit/domaintest.php
+++ tests/Unit/domaintest.php
@@ -2,23 +2,11 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b13ae59 (.)
 use Tests\TestCase;
 use Modules\Tenant\Actions\Domains\GetDomainsArrayAction;
 use Modules\Tenant\Models\Domain;
 
 uses(TestCase::class);
-<<<<<<< HEAD
-=======
-=======
-use Modules\Tenant\Models\Domain;
-
-uses(Tests\TestCase::class);
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
 
 test('domain model can be instantiated', function (): void {
     $domain = new Domain();
@@ -28,29 +16,9 @@ test('domain model can be instantiated', function (): void {
 
 test('get rows method works correctly', function (): void {
     // Mock della Action GetDomainsArrayAction
-<<<<<<< HEAD
     $this->mock(GetDomainsArrayAction::class, function ($mock) {
         $mock
             ->shouldReceive('execute')
-=======
-<<<<<<< HEAD
-    $this->mock(GetDomainsArrayAction::class, function ($mock) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        $mock
-            ->shouldReceive('execute')
-=======
-        $mock->shouldReceive('execute')
->>>>>>> a12f125f4a (.)
-=======
-        $mock
-            ->shouldReceive('execute')
->>>>>>> b93ef594b4 (.)
-=======
-    $this->mock(\Modules\Tenant\Actions\Domains\GetDomainsArrayAction::class, function ($mock) {
-        $mock->shouldReceive('execute')
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
             ->once()
             ->andReturn([
                 ['id' => 1, 'name' => 'test-domain.com'],
