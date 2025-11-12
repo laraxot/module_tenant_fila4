3128026d1f93b18bb627b8f95744358bc81ad641
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:23:29 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 declare(strict_types=1);
 
 /*
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> b13ae59 (.)
  * |--------------------------------------------------------------------------
  * | Web Routes
  * |--------------------------------------------------------------------------
@@ -12,8 +20,43 @@ declare(strict_types=1);
  * | contains the "web" middleware group. Now create something great!
  * |
  */
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> b13ae59 (.)
 /*
  * Route::prefix('tenant')->group(function() {
  * Route::get('/', 'TenantController@index');
  * });
  */
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> origin/develop
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::prefix('tenant')->group(function() {
+    Route::get('/', 'TenantController@index');
+});
+*/
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+/*
+ * Route::prefix('tenant')->group(function() {
+ * Route::get('/', 'TenantController@index');
+ * });
+ */
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
