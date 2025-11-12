7350d3ad6cb643269963ed66648e4e0cb6e41fc9
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:22:37 +0100

    .

--- database/factories/BaseModelJsonsFactory.php
+++ database/factories/BaseModelJsonsFactory.php
@@ -8,14 +8,30 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\Tenant\Models\BaseModelJsons;
 
 /**
+<<<<<<< HEAD
  * @extends Factory<BaseModelJsons>
+=======
+<<<<<<< HEAD
+ * @extends Factory<BaseModelJsons>
+=======
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Tenant\Models\BaseModelJsons>
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
  */
 class BaseModelJsonsFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
+<<<<<<< HEAD
      * @var class-string<BaseModelJsons>
+=======
+<<<<<<< HEAD
+     * @var class-string<BaseModelJsons>
+=======
+     * @var class-string<\Modules\Tenant\Models\BaseModelJsons>
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
      */
     protected $model = BaseModelJsons::class;
 
@@ -33,4 +49,20 @@ class BaseModelJsonsFactory extends Factory
             'updated_by' => $this->faker->uuid(),
         ];
     }
+<<<<<<< HEAD
 }
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> a12f125f4a (.)
+=======
+}
+>>>>>>> b93ef594b4 (.)
+=======
+}
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
