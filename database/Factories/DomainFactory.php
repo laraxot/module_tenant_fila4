6bb72ca69c8ce95910291647cab5c1f37a81e906
marco76tv <marco.sottana@gmail.com>
2025-11-19 15:34:17 +0100

    .

--- database/Factories/DomainFactory.php
+++ database/Factories/DomainFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Modules\Tenant\Database\Factories;
@@ -10,17 +11,28 @@ use Modules\Tenant\Models\Domain;
 /**
  * @extends Factory<Domain>
  */
+=======
+namespace Modules\Tenant\Database\Factories;
+
+use Modules\Tenant\Models\Domain;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+>>>>>>> 7e38bd4 (.)
 class DomainFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
+<<<<<<< HEAD
      *
      * @var class-string<Domain>
+=======
+>>>>>>> 7e38bd4 (.)
      */
     protected $model = Domain::class;
 
     /**
      * Define the model's default state.
+<<<<<<< HEAD
      *
      * @return array<string, mixed>
      */
@@ -64,5 +76,11 @@ class DomainFactory extends Factory
         return $this->state(fn(array $_attributes) => [
             'is_ssl_enabled' => true,
         ]);
+=======
+     */
+    public function definition(): array
+    {
+        return [];
+>>>>>>> 7e38bd4 (.)
     }
 }
