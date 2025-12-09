9aa95a5316df65d045712128c0a6f6b3cb962a23
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:38:45 +0100

    .

--- tests/Unit/Traits/SushiToJsonTest.php
+++ tests/Unit/Traits/SushiToJsonTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Tests\Unit\Traits;
 
+use function Safe\json_decode;
+use function Safe\json_encode;
+
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Mockery;
@@ -275,7 +279,7 @@ class SushiToJsonTest extends TestCase
         Auth::shouldReceive('id')->andReturn(456);
 
         // Crea un nuovo modello
-        $newModel = new TestSushiModel;
+        $newModel = new TestSushiModel();
         $newModel->name = 'New Item';
         $newModel->description = 'New Description';
 
@@ -318,7 +322,7 @@ class SushiToJsonTest extends TestCase
         Auth::shouldReceive('id')->andReturn(789);
 
         // Carica il modello esistente
-        $existingModel = new TestSushiModel;
+        $existingModel = new TestSushiModel();
         $existingModel->id = 1;
         $existingModel->name = 'Updated Name';
         $existingModel->description = 'Updated Description';
@@ -349,7 +353,7 @@ class SushiToJsonTest extends TestCase
         File::put($this->testJsonPath, json_encode($testData, JSON_PRETTY_PRINT));
 
         // Carica il modello da eliminare
-        $modelToDelete = new TestSushiModel;
+        $modelToDelete = new TestSushiModel();
         $modelToDelete->id = 1;
 
         // Simula l'evento deleting
