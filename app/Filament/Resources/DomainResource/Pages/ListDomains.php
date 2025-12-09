311bdb52a76b496aeeaaec74d4333406def07204
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:38:19 +0100

    .

--- app/Filament/Resources/DomainResource/Pages/ListDomains.php
+++ app/Filament/Resources/DomainResource/Pages/ListDomains.php
@@ -15,9 +15,6 @@ class ListDomains extends XotBaseListRecords
     protected static string $resource = DomainResource::class;
 
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableColumns(): array
     {
         return [
