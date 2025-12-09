243e045dfa2e4c816f0bb460495625a863ab6f01
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:36:20 +0100

    .

--- app/Actions/Domains/GetDomainsArrayAction.php
+++ app/Actions/Domains/GetDomainsArrayAction.php
@@ -7,6 +7,7 @@ namespace Modules\Tenant\Actions\Domains;
 // use Illuminate\Support\Facades\File;
 // use Illuminate\Support\Facades\Storage;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Spatie\QueueableAction\QueueableAction;
 
@@ -14,47 +15,24 @@ class GetDomainsArrayAction
 {
     use QueueableAction;
 
-    /**
-     * @return array<string, mixed>
-     */
     public function execute(): array
     {
         $res = $this->recurse(config_path());
         $res1 = $this->collapse($res);
-<<<<<<< HEAD
-        $res2 = [];
-        foreach ($res1 as $key => $value) {
-            $keyStr = is_string($key) ? $key : (string) $key;
-            $res2[$keyStr] = [
-                'id' => $value,
-                'name' => $value,
-            ];
-        }
-=======
         $res2 = Arr::map($res1, fn (string $value) => [
             'id' => $value,
             'name' => $value,
         ]);
->>>>>>> 0f9bf43 (.)
 
         return $res2;
     }
 
-    /**
-     * @return array<string, mixed>
-     */
     public function recurse(string $path): array
     {
         $filesystem = new Filesystem;
         $directories = $filesystem->directories($path);
         $res = [];
         foreach ($directories as $dir) {
-<<<<<<< HEAD
-            if (! is_string($dir)) {
-                continue;
-            }
-=======
->>>>>>> 0f9bf43 (.)
             $name = Str::after($dir, $path.'/');
             if (\in_array($name, ['lang'], true)) {
                 continue;
@@ -65,30 +43,16 @@ class GetDomainsArrayAction
         return $res;
     }
 
-    /**
-     * @return array<string, mixed>
-     */
     public function collapse(array $data, string $k = ''): array
     {
         $res = [];
         foreach ($data as $k0 => $v0) {
-<<<<<<< HEAD
-            $k0Str = is_string($k0) ? $k0 : (string) $k0;
-            $newkey = $k === '' ? $k0Str : ($k0Str.'.'.$k);
-=======
             $newkey = $k === '' ? $k0 : ($k0.'.'.$k);
->>>>>>> 0f9bf43 (.)
             if ($v0 === []) {
-                $res[$newkey] = $newkey;
+                $res[] = $newkey;
             }
 
-            if (is_array($v0)) {
-                $collapsed = $this->collapse($v0, $newkey);
-                foreach ($collapsed as $ck => $cv) {
-                    $ckStr = is_string($ck) ? $ck : (string) $ck;
-                    $res[$ckStr] = $cv;
-                }
-            }
+            $res = array_merge($res, $this->collapse($v0, $newkey));
         }
 
         return $res;
