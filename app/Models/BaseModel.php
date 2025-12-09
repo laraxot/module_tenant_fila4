1f5ea4ff81ceedee47ad3fad5f490010ac558660
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:37:12 +0100

    .

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -75,10 +75,14 @@ abstract class BaseModel extends EloquentModel
             'uuid' => 'string',
             'published_at' => 'datetime',
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 764bbef (.)
             'verified_at' => 'datetime',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
+<<<<<<< HEAD
 =======
 
             'verified_at' => 'datetime',
@@ -88,6 +92,8 @@ abstract class BaseModel extends EloquentModel
             'deleted_at' => 'datetime',
 
 >>>>>>> 15079c8 (.)
+=======
+>>>>>>> 764bbef (.)
             'updated_by' => 'string',
             'created_by' => 'string',
             'deleted_by' => 'string',
