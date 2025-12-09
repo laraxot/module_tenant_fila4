10a6e7da1d91e72b185948148bf09b880b5fe433
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:36:09 +0100

    .

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
-$finder = Symfony\Component\Finder\Finder::create()
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder\Finder;
+
+$finder = Finder::create()
     ->in([
         __DIR__ . '/src',
         __DIR__ . '/tests',
@@ -12,7 +15,7 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return new PhpCsFixer\Config()->setRules([
+return (new Config())->setRules([
     '@PSR12' => true,
     'array_syntax' => ['syntax' => 'short'],
     'ordered_imports' => ['sort_algorithm' => 'alpha'],
@@ -36,7 +39,6 @@ return new PhpCsFixer\Config()->setRules([
         'on_multiline' => 'ensure_fully_multiline',
         'keep_multiple_spaces_after_comma' => true,
     ],
-    ,
     'braces' => [
         'position_after_functions_and_oop_constructs' => 'same',
     ],
