c8f70b3d714be85cc152ba3f7086eaa2ed4c27e7
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:10:30 +0100

    .

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -25,9 +25,9 @@ use Sushi\Sushi;
  *
  * @method static DomainFactory factory($count = null, $state = [])
  *
- * @mixin IdeHelperDomain
  * @mixin \Eloquent
  */
+/** */
 class Domain extends BaseModel
 {
     use Sushi;
