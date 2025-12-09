c4c95a858f3e2d029e9f6b6c6efc5b406137150f
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:35:01 +0100

    .

--- app/Models/Traits/SushiToPhpArray.php
+++ app/Models/Traits/SushiToPhpArray.php
@@ -8,14 +8,7 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Models\Traits;
 
-<<<<<<< HEAD
 use Sushi\Sushi;
-=======
-<<<<<<< HEAD
-use Sushi\Sushi;
-=======
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Modules\Tenant\Services\TenantService;
@@ -24,15 +17,7 @@ use function Safe\json_encode;
 
 trait SushiToPhpArray
 {
-<<<<<<< HEAD
     use Sushi;
-=======
-<<<<<<< HEAD
-    use Sushi;
-=======
-    use \Sushi\Sushi;
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
 
     public function getSushiRows(): array
     {
@@ -43,12 +28,6 @@ trait SushiToPhpArray
         $items = array_values($rows);
 
         return $items;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> b13ae59 (.)
 
         /*
          * $files = File::glob($path.'/*.json');
@@ -68,55 +47,6 @@ trait SushiToPhpArray
          *
          * return $rows;
          */
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-        /*
-        $files = File::glob($path.'/*.json');
-        $rows = [];
-        foreach ($files as $id => $file) {
-            $json = File::json($file);
-            $item = [];
-            foreach ($this->schema as $name => $type) {
-                $value = $json[$name] ?? null;
-                if (is_array($value)) {
-                    $value = json_encode($value, JSON_PRETTY_PRINT);
-                }
-                $item[$name] = $value;
-            }
-            $rows[] = $item;
-        }
-
-        return $rows;
-        */
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-
-        /*
-         * $files = File::glob($path.'/*.json');
-         * $rows = [];
-         * foreach ($files as $id => $file) {
-         * $json = File::json($file);
-         * $item = [];
-         * foreach ($this->schema as $name => $type) {
-         * $value = $json[$name] ?? null;
-         * if (is_array($value)) {
-         * $value = json_encode($value, JSON_PRETTY_PRINT);
-         * }
-         * $item[$name] = $value;
-         * }
-         * $rows[] = $item;
-         * }
-         *
-         * return $rows;
-         */
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
     }
 
     /**
@@ -128,12 +58,6 @@ trait SushiToPhpArray
          * During a model create Eloquent will also update the updated_at field so
          * need to have the updated_by field here as well.
          */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> b13ae59 (.)
         static::creating(function ($model): void {
             // Arr::keyBy($array,
 
@@ -145,114 +69,20 @@ trait SushiToPhpArray
         static::updating(function ($model): void {
             dd($model->toArray());
         });
-<<<<<<< HEAD
-=======
-=======
-        static::creating(
-            function ($model): void {
-                // Arr::keyBy($array,
-=======
-        static::creating(function ($model): void {
-            // Arr::keyBy($array,
->>>>>>> b93ef594b4 (.)
-
-            dd($model->toArray());
-        });
-        /*
-         * updating.
-         */
-<<<<<<< HEAD
-=======
-        static::creating(
-            function ($model): void {
-                // Arr::keyBy($array,
-
-                dd($model->toArray());
-            }
-        );
-        /*
-         * updating.
-         */
->>>>>>> origin/develop
-        static::updating(
-            function ($model): void {
-                dd($model->toArray());
-            }
-        );
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-        static::updating(function ($model): void {
-            dd($model->toArray());
-        });
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
         // -------------------------------------------------------------------------------------
         /*
          * Deleting a model is slightly different than creating or deleting.
          * For deletes we need to save the model first with the deleted_by field
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> b13ae59 (.)
          */
 
         static::deleting(function ($_model): void {
             dd('WIP');
         });
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-        */
-
-        static::deleting(
-            function ($model): void {
-                dd('WIP');
-            }
-        );
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-         */
-
-        static::deleting(function ($_model): void {
-            dd('WIP');
-        });
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
 
         // ----------------------
     }
 
     // end function boot
-<<<<<<< HEAD
 }
 
 // end trait Updater
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-
-// end trait Updater
-=======
-}// end trait Updater
->>>>>>> a12f125f4a (.)
-=======
-}
-
-// end trait Updater
->>>>>>> b93ef594b4 (.)
-=======
-}// end trait Updater
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -2,60 +2,17 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> b13ae59 (.)
 namespace Modules\Tenant\Tests\Unit\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\Tenant\Models\BaseModel;
-<<<<<<< HEAD
-=======
-=======
-namespace Modules\Tenant\Tests\Unit\Models;
-
-use Modules\Tenant\Models\BaseModel;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Database\Eloquent\Model;
->>>>>>> origin/develop
 use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class);
 
 beforeEach(function () {
     $this->baseModel = new class extends BaseModel {
-<<<<<<< HEAD
-=======
-=======
-namespace Modules\Tenant\Tests\Unit\Models;
-
->>>>>>> b93ef594b4 (.)
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Modules\Tenant\Models\BaseModel;
->>>>>>> b13ae59 (.)
-use Tests\TestCase;
-
-uses(TestCase::class, RefreshDatabase::class);
-
-beforeEach(function () {
-<<<<<<< HEAD
-    $this->baseModel = new class extends BaseModel {
-=======
-<<<<<<< HEAD
-    $this->baseModel = new class() extends BaseModel
-    {
->>>>>>> a12f125f4a (.)
-=======
-    $this->baseModel = new class extends BaseModel {
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
         protected $table = 'test_tenant_table';
     };
 });
