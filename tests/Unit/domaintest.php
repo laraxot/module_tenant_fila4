bd5b1e28b79061c64dbb813e8e8cc38cd99dba05
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:41:46 +0100

    .

--- tests/Unit/domaintest.php
+++ tests/Unit/domaintest.php
@@ -9,11 +9,7 @@ use Modules\Tenant\Models\Domain;
 uses(TestCase::class);
 
 test('domain model can be instantiated', function (): void {
-<<<<<<< HEAD
-    $domain = new Domain;
-=======
     $domain = new Domain();
->>>>>>> bbe0788 (.)
 
     expect($domain)->toBeInstanceOf(Domain::class);
 });
@@ -21,12 +17,8 @@ test('domain model can be instantiated', function (): void {
 test('get rows method works correctly', function (): void {
     // Mock della Action GetDomainsArrayAction
     $this->mock(GetDomainsArrayAction::class, function ($mock) {
-<<<<<<< HEAD
-        $mock->shouldReceive('execute')
-=======
         $mock
             ->shouldReceive('execute')
->>>>>>> bbe0788 (.)
             ->once()
             ->andReturn([
                 ['id' => 1, 'name' => 'test-domain.com'],
@@ -34,11 +26,7 @@ test('get rows method works correctly', function (): void {
             ]);
     });
 
-<<<<<<< HEAD
-    $domain = new Domain;
-=======
     $domain = new Domain();
->>>>>>> bbe0788 (.)
     $rows = $domain->getRows();
 
     expect($rows)->toBeArray();
