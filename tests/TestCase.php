34678fc019d6c37600f4efe04d0c2b94b23168c4
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:17:00 +0100

    .

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,7 +24,6 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         // Load Tenant module specific configurations
-        /** @phpstan-ignore-next-line method.notFound */
         $this->loadLaravelMigrations();
 
         // Seed any required data for Tenant tests
