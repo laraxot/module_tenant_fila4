5b0cb6eff14778820a3bc2519394c8e745d7a361
marco76tv <marco.sottana@gmail.com>
2025-12-10 01:50:00 +0100

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 764bbef (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 15079c8 (.)
-=======
->>>>>>> 764bbef (.)
 @extends('tenant::layouts.master')
 
 @section('content')
