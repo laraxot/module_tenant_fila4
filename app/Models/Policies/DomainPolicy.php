7f49f4e69a015918b125c5642177f7cd521460ed
marco76tv <marco.sottana@gmail.com>
2025-12-10 01:41:28 +0100

    .

--- app/Models/Policies/DomainPolicy.php
+++ app/Models/Policies/DomainPolicy.php
@@ -5,63 +5,249 @@ declare(strict_types=1);
 namespace Modules\Tenant\Models\Policies;
 
 use Modules\Tenant\Models\Domain;
+<<<<<<< HEAD
 use Modules\Xot\Contracts\UserContract;
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+use Modules\Xot\Contracts\UserContract;
+=======
+use Modules\Xot\Contracts\ProfileContract;
+>>>>>>> a12f125f4a (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> b93ef594b4 (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
 
 class DomainPolicy extends TenantBasePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
+<<<<<<< HEAD
     public function viewAny(UserContract $user): bool
     {
         return $user->hasPermissionTo('domain.viewAny');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.viewAny');
+=======
+    public function viewAny(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.viewAny'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.viewAny');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.viewAny');
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     }
 
     /**
      * Determine whether the user can view the model.
      */
+<<<<<<< HEAD
     public function view(UserContract $user, Domain $_domain): bool
     {
         return $user->hasPermissionTo('domain.view');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function view(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.view');
+=======
+    public function view(ProfileContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.view'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function view(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.view');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function view(UserContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.view');
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     }
 
     /**
      * Determine whether the user can create models.
      */
+<<<<<<< HEAD
     public function create(UserContract $user): bool
     {
         return $user->hasPermissionTo('domain.create');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.create');
+=======
+    public function create(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.create'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.create');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('domain.create');
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     }
 
     /**
      * Determine whether the user can update the model.
      */
+<<<<<<< HEAD
     public function update(UserContract $user, Domain $_domain): bool
     {
         return $user->hasPermissionTo('domain.update');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function update(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.update');
+=======
+    public function update(ProfileContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.update'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function update(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.update');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function update(UserContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.update');
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
     public function delete(UserContract $user, Domain $_domain): bool
     {
         return $user->hasPermissionTo('domain.delete');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function delete(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.delete');
+=======
+    public function delete(ProfileContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.delete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function delete(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.delete');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function delete(UserContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.delete');
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
     public function restore(UserContract $user, Domain $_domain): bool
     {
         return $user->hasPermissionTo('domain.restore');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function restore(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.restore');
+=======
+    public function restore(ProfileContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.restore'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function restore(UserContract $user, Domain $_domain): bool
+    {
+        return $user->hasPermissionTo('domain.restore');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function restore(UserContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.restore');
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function forceDelete(UserContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.forceDelete');
+=======
+    public function forceDelete(ProfileContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.forceDelete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function forceDelete(UserContract $user, Domain $domain): bool
+    {
+        return $user->hasPermissionTo('domain.forceDelete');
+>>>>>>> b93ef594b4 (.)
+    }
+}
+=======
+>>>>>>> b13ae59 (.)
     public function forceDelete(UserContract $user, Domain $domain): bool
     {
         return $user->hasPermissionTo('domain.forceDelete');
     }
 }
+<<<<<<< HEAD
+=======
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
