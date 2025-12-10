9a6e73e891f77bfd48017e5c5566626292c6e7c1
marco76tv <marco.sottana@gmail.com>
2025-12-10 01:51:26 +0100

    .

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -8,19 +8,7 @@ use Modules\Xot\Filament\Pages\XotBaseDashboard;
 
 class Dashboard extends XotBaseDashboard
 {
-<<<<<<< HEAD
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-home';
 
     protected string $view = 'tenant::filament.pages.dashboard';
-=======
-<<<<<<< HEAD
-    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-home';
-
-    protected string $view = 'tenant::filament.pages.dashboard';
-=======
-    protected static ?string $navigationIcon = 'heroicon-o-home';
-
-    protected static string $view = 'tenant::filament.pages.dashboard';
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
 }
