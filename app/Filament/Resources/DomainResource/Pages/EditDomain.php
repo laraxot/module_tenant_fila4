ed72aede34e6e83ac0288e33553dc20504175871
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:09:46 +0100

    .

--- app/Filament/Resources/DomainResource/Pages/EditDomain.php
+++ app/Filament/Resources/DomainResource/Pages/EditDomain.php
@@ -7,15 +7,7 @@ namespace Modules\Tenant\Filament\Resources\DomainResource\Pages;
 use Modules\Tenant\Filament\Resources\DomainResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
-<<<<<<< HEAD
 class EditDomain extends XotBaseEditRecord
-=======
-<<<<<<< HEAD
-class EditDomain extends XotBaseEditRecord
-=======
-class EditDomain extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
->>>>>>> origin/develop
->>>>>>> b13ae59 (.)
 {
     protected static string $resource = DomainResource::class;
 }
