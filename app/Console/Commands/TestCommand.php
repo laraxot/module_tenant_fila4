0bf8a2dfe0b98152f9ec8455994a5d91e712674a
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:58:08 +0100

    .

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -19,11 +19,6 @@ class TestCommand extends Command
      * Execute the console command.
      */
     public function handle(): void
-<<<<<<< HEAD
-=======
-    public function handle()
->>>>>>> f057083 (.)
->>>>>>> 62af1b1 (.)
     {
         $name = TenantService::getName();
         $this->info('tenant name :'.$name);
