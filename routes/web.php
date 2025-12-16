cbb562551105f233b8bf9dbad69c038ca236a245
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:56:13 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 /*
+<<<<<<< HEAD
  * |--------------------------------------------------------------------------
  * | Web Routes
  * |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ declare(strict_types=1);
  * Route::get('/', 'TenantController@index');
  * });
  */
+=======
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::prefix('tenant')->group(function() {
+    Route::get('/', 'TenantController@index');
+});
+*/
+>>>>>>> 15079c8 (.)
