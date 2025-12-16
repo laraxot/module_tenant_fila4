baabd7d1f3f8437c133c1da17d92c3e453de47e6
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:52:34 +0100

    .

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 $finder = Symfony\Component\Finder\Finder::create()
     ->in([
+<<<<<<< HEAD
         __DIR__ . '/src',
         __DIR__ . '/tests',
+=======
+        __DIR__.'/src',
+        __DIR__.'/tests',
+>>>>>>> 15079c8 (.)
     ])
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
+<<<<<<< HEAD
 return new PhpCsFixer\Config()->setRules([
     '@PSR12' => true,
     'array_syntax' => ['syntax' => 'short'],
@@ -42,3 +48,37 @@ return new PhpCsFixer\Config()->setRules([
     ],
     'single_trait_insert_per_statement' => true,
 ])->setFinder($finder);
+=======
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'trailing_comma_in_multiline' => true,
+        'phpdoc_scalar' => true,
+        'unary_operator_spaces' => true,
+        'binary_operator_spaces' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+        ],
+        'phpdoc_single_line_var_spacing' => true,
+        'phpdoc_var_without_name' => true,
+        'class_attributes_separation' => [
+            'elements' => [
+                'method' => 'one',
+            ],
+        ],
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_fully_multiline',
+            'keep_multiple_spaces_after_comma' => true,
+        ],
+        ,
+        'braces' => [
+            'position_after_functions_and_oop_constructs' => 'same',
+        ],
+        'single_trait_insert_per_statement' => true,
+    ])
+    ->setFinder($finder);
+>>>>>>> 15079c8 (.)
