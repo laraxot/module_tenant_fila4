9c111c9e51250c173e241465fc31210c193d7660
marco76tv <marco.sottana@gmail.com>
2025-12-16 22:33:54 +0100

    .

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Modules\Tenant\Models\BaseModel;
 
 beforeEach(function () {
-    $this->baseModel = new class extends BaseModel
-    {
+    $this->baseModel = new class () extends BaseModel {
         protected $table = 'test_tenant_table';
     };
 });
