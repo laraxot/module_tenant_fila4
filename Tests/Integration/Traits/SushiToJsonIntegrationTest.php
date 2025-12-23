c64898156d85964f5d5245622158a3ee6847491a
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:50:30 +0100

    .

--- Tests/Integration/Traits/SushiToJsonIntegrationTest.php
+++ Tests/Integration/Traits/SushiToJsonIntegrationTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Tests\Integration\Traits;
 
+use function Safe\json_decode;
+use function Safe\json_encode;
+
+
 use Illuminate\Support\Facades\File;
 use Modules\Tenant\Models\Tenant;
 use Modules\Tenant\Models\TestSushiModel;
