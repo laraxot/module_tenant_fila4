1190470a9f3316dcb7782d0f5eaa804a57505532
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:46:40 +0100

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> b13ae59 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> b13ae59 (.)
 @extends('tenant::layouts.master')
 
 @section('content')
