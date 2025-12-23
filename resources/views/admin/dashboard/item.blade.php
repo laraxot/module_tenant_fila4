3c3dfd2f45761c118332c7ad15e2096ebae6c955
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:43:43 +0100

    .

--- resources/views/admin/dashboard/item.blade.php
+++ resources/views/admin/dashboard/item.blade.php
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 764bbef (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
 <!-- dashboard item -->
-<<<<<<< HEAD
-=======
-<!-- dashboard item -->
->>>>>>> 15079c8 (.)
-=======
->>>>>>> 764bbef (.)
